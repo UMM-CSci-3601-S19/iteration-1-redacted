de6233887dbfbf0351f426028e8e253fff55ec4b
deglm006 <deglm006@morris.umn.edu>
2019-03-10 22:13:43 -0500

    Create test for adding a ride #18

--- client/src/app/rides/ride-list.component.spec.ts
+++ client/src/app/rides/ride-list.component.spec.ts
@@ -82,67 +82,78 @@ describe('Misbehaving Ride List', () => {
 });
 
 
-// describe('Adding a ride', () => {
-//   let rideList: RideListComponent;
-//   let fixture: ComponentFixture<RideListComponent>;
-//   const newRide: Ride ;
-//   const newId ;
-//
-//   let calledRide: Ride;
-//
-//   let rideListServiceStub: {
-//     getRides: () => Observable<Ride[]>,
-//     addNewRide: (newRide: Ride) => Observable<{ '$oid': string }>
-//   };
-//   let mockMatDialog: {
-//     open: (AddRideComponent, any) => {
-//       afterClosed: () => Observable<Ride>
-//     };
-//   };
-//
-//   beforeEach(() => {
-//     calledRide = null;
-//     // stub RideService for test purposes
-//     rideListServiceStub = {
-//       getRides: () => Observable.of([]),
-//       addNewRide: (newRide: Ride) => {
-//         calledRide = newRide;
-//         return Observable.of({
-//           '$oid': newId
-//         });
-//       }
-//     };
-//     mockMatDialog = {
-//       open: () => {
-//         return {
-//           afterClosed: () => {
-//             return Observable.of(newRide);
-//           }
-//         };
-//       }
-//     };
-//
-//     TestBed.configureTestingModule({
-//       imports: [FormsModule, CustomModule],
-//       declarations: [RideListComponent],
-//       providers: [
-//         {provide: RideListService, useValue: rideListServiceStub},
-//         {provide: MatDialog, useValue: mockMatDialog},
-//       ]
-//     });
-//   });
-//
-//   beforeEach(async(() => {
-//     TestBed.compileComponents().then(() => {
-//       fixture = TestBed.createComponent(RideListComponent);
-//       rideList = fixture.componentInstance;
-//       fixture.detectChanges();
-//     });
-//   }));
-//
-//   it('calls RideListService.addRide', () => {
-//     expect(calledRide).toBeNull();
-//     //rideList.openDialog();
-//     expect(calledRide).toEqual(newRide);
-//   });
-// });
+describe('Adding a ride', () => {
+  let rideList: RideListComponent;
+  let fixture: ComponentFixture<RideListComponent>;
+  const newRide: Ride = {
+    _id: '',
+    driver: 'Arlene Talley',
+    riders: [],
+    route: [
+      "Elfrida",
+      "Alden"
+    ],
+    roundTrip: false,
+    dateTime: 'Thu Aug 29 1985 23:08:05 GMT+0000 (UTC)',
+    notes: 'Eiusmod ad ipsum laboris amet aute reprehenderit commodo commodo.'
+  };
+  const newId = 'arlene_id';
+
+  let calledRide: Ride;
+
+  let rideListServiceStub: {
+    getRides: () => Observable<Ride[]>,
+    addNewRide: (newRide: Ride) => Observable<{ '$oid': string }>
+  };
+  let mockMatDialog: {
+    open: (AddRideComponent, any) => {
+      afterClosed: () => Observable<Ride>
+    };
+  };
+
+  beforeEach(() => {
+    calledRide = null;
+    // stub RideService for test purposes
+    rideListServiceStub = {
+      getRides: () => Observable.of([]),
+      addNewRide: (newRide: Ride) => {
+        calledRide = newRide;
+        return Observable.of({
+          '$oid': newId
+        });
+      }
+    };
+    mockMatDialog = {
+      open: () => {
+        return {
+          afterClosed: () => {
+            return Observable.of(newRide);
+          }
+        };
+      }
+    };
+
+    TestBed.configureTestingModule({
+      imports: [FormsModule, CustomModule],
+      declarations: [RideListComponent],
+      providers: [
+        {provide: RideListService, useValue: rideListServiceStub},
+        {provide: MatDialog, useValue: mockMatDialog},
+      ]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(RideListComponent);
+      rideList = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('calls RideListService.addRide', () => {
+    expect(calledRide).toBeNull();
+    rideList.openDialog();
+    expect(calledRide).toEqual(newRide);
+  });
+});
