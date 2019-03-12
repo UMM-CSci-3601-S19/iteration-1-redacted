4ae42e17b272b61b7a7f86d0c2be2bc37c907c25
deglm006 <deglm006@morris.umn.edu>
2019-03-12 14:34:54 -0500

    Update home page to include body text

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,11 +2,14 @@ import {Component} from '@angular/core';
 
 @Component({
   templateUrl: 'home.component.html'
+
 })
 export class HomeComponent {
-  public text: string;
+  public header: string;
+  public body: string;
 
   constructor() {
-    this.text = 'MoRide';
+    this.header = 'MoRide';
+    this.body = 'Welcome to MoRide!  Helping people carpool since 2019!'
   }
 }
