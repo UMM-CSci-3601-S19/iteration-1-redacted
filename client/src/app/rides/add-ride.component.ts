4cd0d8a4ac9433fcf4330ae2d77f09500d92b68a
JReuvers <reuve026@morris.umn.edu>
2019-03-11 19:43:20 -0500

    Implements matDatePicker with proper imports and html stuff

--- client/src/app/rides/add-ride.component.ts
+++ client/src/app/rides/add-ride.component.ts
@@ -1,7 +1,15 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material';
+import {Component, Inject, NgModule, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDatepickerModule, MatNativeDateModule} from '@angular/material';
 import {Ride} from './ride';
 import {FormControl, Validators, FormGroup, FormBuilder} from "@angular/forms";
+
+@NgModule({
+  imports:
+    [
+      MatDatepickerModule,
+      MatNativeDateModule,
+    ]
+})
 
 @Component({
   selector: 'add-ride.component',
@@ -29,7 +37,8 @@ export class AddRideComponent implements OnInit {
     this.addRideForm = this.fb.group({
 
       driver: new FormControl('driver'),
-      notes: new FormControl('notes')
+      notes: new FormControl('notes'),
+      dateTime: new FormControl('dateTime')
 
     })
 
